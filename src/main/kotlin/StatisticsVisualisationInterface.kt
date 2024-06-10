fe00725f37d893a4cc6a76a9593e6fb5b8e30d33
Taofeek Alao <taofeekalao@gmail.com>
2024-06-10 19:36:36 +0100

    Introduced blank interface contract and blank implementation for Histogram visualisation.

---
+++ src/main/kotlin/StatisticsVisualisationInterface.kt
@@ -0,0 +1,11 @@
+package com.metric.search.visualisatioin
+
+/**
+ * This histogram interface defines the contract for the visualisation of histograms.
+ */
+interface Histogram
+
+/**
+ * This is the implementation class of the Histogram interface.
+ */
+class HistogramImpl() : Histogram
