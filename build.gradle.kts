af85b70c56301f2dd08214ee47438ad82292e2f5
Taofeek Alao <taofeekalao@gmail.com>
2024-06-10 07:16:04 +0100

    Initialised the repository and added the dependencies for Kotlin Kandy.

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "com.metric.search.visualisatioin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://packages.jetbrains.team/maven/p/kds/kotlin-ds-maven")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(files("libs/Metric-space-framework-1.0-20211209.152843-1.jar"))
+    implementation("org.jetbrains.kotlinx:kandy-lets-plot:0.6.0")
+    implementation("org.jetbrains.kotlinx:kotlin-statistics-jvm:0.2.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
