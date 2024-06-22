2b04fa32f268b3e9f37d1aab9b0fab947f05baa3
Taofeek Alao <taofeekalao@gmail.com>
2024-06-22 20:31:56 +0100

    Removed all fat configuration and stabilised the library for use in other devs and Kotlin Notebook.
    Removed all fat configuration and stabilised the library for use in other devs and Kotlin Notebook.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,19 +7,6 @@ apply(plugin = "maven-publish")
 group = "com.metric.search.visualisation"
 version = "1.0-SNAPSHOT"
 
-tasks.register<Jar>("fatJar") {
-    archiveClassifier.set("all")
-    from(sourceSets.main.get().output)
-
-    dependsOn(configurations.runtimeClasspath)
-    from({
-        configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
-    })
-
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE // Handle duplicate files by excluding them
-}
-
-
 repositories {
     mavenCentral()
     maven("https://packages.jetbrains.team/maven/p/kds/kotlin-ds-maven")
@@ -27,16 +14,7 @@ repositories {
 
 dependencies {
     implementation(files("libs/Metric-space-framework-1.0-20211209.152843-1.jar"))
-    // https://mvnrepository.com/artifact/org.jetbrains.lets-plot/lets-plot-kotlin-api
-//    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-api:2.0.1")
-
-//    implementation("org.jetbrains.kotlinx:kandy-api:0.6.0-dev-48")
-//    implementation("org.jetbrains.kotlinx:kandy-lets-plot:0.6.0")
     implementation("org.jetbrains.kotlinx:kotlin-statistics-jvm:0.2.1")
-//    implementation("org.jetbrains.lets-plot:lets-plot-common:2.1.0")
-
-    // https://mvnrepository.com/artifact/org.jetbrains.lets-plot/lets-plot-kotlin-jvm
-//    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.7.3")
 }
 
 tasks.test {
@@ -51,13 +29,7 @@ configure<PublishingExtension> {
     publications {
         create<MavenPublication>("maven") {
             artifactId = "visualisation-library"
-//            from(components["java"])
             from(components["kotlin"])
-
-            // Include the fat JAR in the publication
-            artifact(tasks.named("fatJar").get()) {
-                classifier = "all"
-            }
         }
     }
 }
