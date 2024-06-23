fe4d2fa8ee4695d41d7cfa523838249f390601a4
Taofeek Alao <taofeekalao@gmail.com>
2024-06-23 21:50:55 +0100

    Added the initial implementation of the Scatter Plot wrapper.
    Added the initial implementation of the Scatter Plot wrapper using the Kotlin Kandy Lets-Plot engine.

--- src/main/kotlin/Scatter.kt
+++ src/main/kotlin/Scatter.kt
@@ -3,32 +3,37 @@ package com.metric.search.visualisation
 import org.jetbrains.kotlinx.kandy.util.color.Color
 import org.jetbrains.kotlinx.kandy.dsl.plot
 import org.jetbrains.kotlinx.kandy.ir.Plot
-import org.jetbrains.kotlinx.statistics.kandy.layers.histogram
+import org.jetbrains.kotlinx.kandy.letsplot.layers.points
+import org.jetbrains.kotlinx.kandy.letsplot.settings.Symbol
 
 /**
- * This is the implementation class of the Histogram.
+ * This is the implementation class of the Scatter Plot.
  */
-class Histogram(
+class Scatter(
     override var yAxisDescription: String = "Y - Axis",
     override var xAxisDescription: String = "X - Axis",
     override var diagramDescription: String = "Histogram diagram",
     override var colour: Color = Color.BLUE,
     override var xDataSet: List<Double>,
-    override var yDataSet: List<Double> = mutableListOf<Double>()
+    override var yDataSet: List<Double>
 ) : TwoDimensionalPlot {
 
     init {
-        if (xDataSet.isEmpty()) {
-            throw IllegalArgumentException("xDataSet can't be empty")
+        if (xDataSet.isEmpty() || yDataSet.isEmpty()) {
+            throw IllegalArgumentException("xDataSet or yDataSet can't be empty")
         }
     }
 
     /**
-     * Function to plot histogram based on Kotlin Kandy
+     * Function to plot scatter diagram based on Kotlin Kandy
      */
     override fun plot(): Plot {
         return plot {
-            histogram(xDataSet)
+            points {
+                x(xDataSet)
+                y(yDataSet)
+                symbol = Symbol.CIRCLE_OPEN
+            }
         }
     }
 }
